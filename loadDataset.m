76a90c0327029e1437c2af86e8e18f1e675fe95a
Leonardo Piano <leopiano.98@gmail.com>
2022-02-10 15:19:11 +0100

    missing

--- loadDataset.m
+++ loadDataset.m
@@ -1,18 +1,18 @@
-function [train_img,test_img]=loadDataset()
+function [train_img,test_img,train_labels,test_labels]=loadDataset()
 
 train_table=readtable('training_list.csv'); %Read train table
 test_table=readtable("validation_list.csv"); %Read val table
 
 train_files=train_table{:,'Var1'}; %Take only the image files
-train_labels=train_table{:,'Var6'}; %Classification Labels
+train_labels=categorical(train_table{:,'Var6'}); %Classification Labels
 train_img=imageDatastore("images\"); %Store dataset on ImageDataStore
 train_img.Files=train_files;
-train_img.Labels=categorical(train_labels);
+train_img.Labels=train_labels;
 
 test_files=test_table{:,'Var1'}; %Take only the image files
-test_labels=test_table{:,'Var6'}; %Classification Labels 
+test_labels=categorical(test_table{:,'Var6'}); %Classification Labels 
 test_img=imageDatastore("images\");
 test_img.Files=test_files;
-test_img.Labels=categorical(test_labels);
+test_img.Labels=test_labels;
 
 end
